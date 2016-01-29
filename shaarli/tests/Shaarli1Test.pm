cbc66a1401a10ecdd7c0e7f9eb093758f82592c9
Johannes Ernst <jernst@indiecomputing.com>
2016-01-29 23:15:55 +0000

    Bump version to get recent commit built Added simple test

---
+++ shaarli/tests/Shaarli1Test.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#
+# Simple test for selfoss
+#
+# Copyright (C) 2016 Indie Computing Corp.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use warnings;
+
+package Shaarli1Test;
+
+use UBOS::WebAppTest;
+
+# The states and transitions for this test
+
+my $TEST = new UBOS::WebAppTest(
+    appToTest => 'shaarli',
+
+    checks => [
+            new UBOS::WebAppTest::StateCheck(
+                    name  => 'virgin',
+                    check => sub {
+                        my $c = shift;
+
+                        $c->getMustStatus( '/', 200, 'Wrong status' );
+                    }
+            )
+    ]
+);
+
+$TEST;
